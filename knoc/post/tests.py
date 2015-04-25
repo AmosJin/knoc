270e87c51255c5aea2e1b30036720b003baa8f36
AmosJin <amosjin@icloud.com>
2015-04-25 23:29:51 +0800

    fix tests indent error, add update fixtures

--- knoc/post/tests.py
+++ knoc/post/tests.py
@@ -7,7 +7,7 @@ from post import core
 
 
 class BasicTest(TestCase):
-    fixtures = ['user.json', 'group.json', 'link.json', 'note.json', 'user_group.json']
+    fixtures = ['user.json', 'group.json', 'link.json', 'note.json', 'user_group.json', 'item.json']
 
     def setUp(self):
         self.user = User.objects.get(pk=1)
@@ -19,23 +19,27 @@ class BasicTest(TestCase):
  
 class LinkInfoTest(BasicTest):
     def test_get_encoding(self):
-        content = '''<!DOCTYPE html>
+		print 'test get_encoding ...'
+		content = """ 
+				<!DOCTYPE html>
                   <html id="spLianghui">\n<head>\n<meta http-equiv="Content-Type" 
                   content="text/html; charset=gb2312" /
-                  '''
+				  """
 
-        encode = core.get_encoding(content)
-        self.assertEqual(encode, "gb2312")
+		encode = core.get_encoding(content)
+		self.assertEqual(encode, "gb2312")
 
     def test_get_domain(self):
-        domain = core.get_url_domain("http://www.shanbay.com/test/more")
-        self.assertEqual("http://www.shanbay.com/", domain)
+		print 'test get_domain ...'
+		domain = core.get_url_domain("http://www.shanbay.com/test/more")
+		self.assertEqual("http://www.shanbay.com/", domain)
 
-        domain = core.get_url_domain("http://www.scipark.net/news/sci_xp542.html")
-        self.assertEqual("http://www.scipark.net/", domain)
+		domain = core.get_url_domain("http://www.scipark.net/news/sci_xp542.html")
+		self.assertEqual("http://www.scipark.net/", domain)
 
     def test_link_info(self):
-        content = '''
+		print 'test link info...'
+		content = """ 
             <html>
                 <head>
                     <title>title</title>
@@ -47,33 +51,37 @@ class LinkInfoTest(BasicTest):
                     <img src="http://qstatic.shanbay.com/static/img/landing_page_logo.png"/>
                 </body>
             </html>
-        '''
-        data = core.get_link_info("http://www.shanbay.com", content)
-        self.assertEqual("title", data["title"])
-        self.assertEqual("description", data["description"])
-        self.assertEqual("http://qstatic.shanbay.com/static/img/landing_page_logo.png", data["image"])
+			"""
+		data = core.get_link_info("http://www.shanbay.com", content)
+		self.assertEqual("title", data["title"])
+		self.assertEqual("description", data["description"])
+		self.assertEqual("http://qstatic.shanbay.com/static/img/landing_page_logo.png", data["image"])
 
 class ItemUpdateTest(BasicTest):
     def setUp(self):
-        super(ItemUpdateTest, self).setUp()
-        self.group = Group.objects.get(pk=1)
+		print 'test ItemUpdate...'
+		super(ItemUpdateTest, self).setUp()
+		self.group = Group.objects.get(pk=1)
 
     def test_create_item(self):
-        user = self.user
-        group = self.group
-        link = Link(title="link title", description="some description", 
+		print 'test create_item...'
+		user = self.user
+		group = self.group
+		link = Link(title="link title", description="some description", 
                 link="http://www.shanbay.com", image="http://qstatic.shanbay.com/static/img/logo2.png")
-        link.save()
-        item = core.update_item(link, user_id=user.pk, group_id=group.pk)
-        self.assertEqual(item.object_id, link.pk)
+		link.save()
+		item = core.update_item(link, user_id=user.pk, group_id=group.pk)
+		self.assertEqual(item.object_id, link.pk)
 
     def test_update_item(self):
-        link = Link.objects.get(id=1)
-        item = core.update_item(link, user_id=self.user.pk, group_id=self.group.id)
-        self.assertEqual(item.object_id, link.id)
+		print 'test update_item...'
+		link = Link.objects.get(id=1)
+		item = core.update_item(link, user_id=self.user.pk, group_id=self.group.id)
+		self.assertEqual(item.object_id, link.id)
 
 class ApiViewTest(BasicTest):
 	def setUp(self):
+		print 'test ApiView...'
 		self.client = Client()
 		self.client.login(username='test_user', password='1234567')
 
@@ -82,16 +90,12 @@ class ApiViewTest(BasicTest):
 		print 'testing group get method...'
 		res = self.client.get('/api/post/group/')
 		self.assertEqual(len(res.data['data']), 4)
+		groups = set(['shanbay', 'py', 'Android', 'iOS'])
+		assert_groups = set([data['name'] for data in res.data['data']])
+		self.assertEqual(groups, assert_groups)
 
-	def test_item_view(self):
+	def est_item_view(self):
 		print 'testing item view...'
 		print 'testing item get method...'
 		res = self.client.get('/api/post/item/1/')
 		self.assertEqual(len(res.data['data']), 4)
-
-	def est_link_view(self):
-		print 'testing link view...'
-		print 'testing group get method...'
-		res = self.client.get('/api/post/link/')
-		self.assertEqual(len(res.data['data']), 4)
-
